133c364d13b4d8e647a85cf842953ce9aec595df
Amartya Chakraborty <amartya.3654@gmail.com>
2023-11-05 11:47:33 +0530

    initial commit

---
+++ src/app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, request
+from frappe_lib import frappe_client
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]=os.getenv("DB_URI")
+
+db = SQLAlchemy()
+db.init_app(app)
+
+
+@app.get("/")
+def ping():
+    return "server is live"
+
+@app.get("/books")
+def books():
+    
+    query=dict()
+    for key in request.args.keys():
+        if request.args.get(key):
+            query.setdefault(key, request.args.get(key)) 
+    return frappe_client.get_books(query)
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    app.run(host="127.0.0.1", debug=True, port=port)
---
+++ src/frappe_lib.py
@@ -0,0 +1,37 @@
+import requests
+
+class FrappeLib:
+    def __init__(self) -> None:
+        self.__client = None
+        self.frappe_url="https://frappe.io/api/method/frappe-library"
+        self.open_lib_url="https://covers.openlibrary.org/b/isbn"
+    # def get_client(self):
+    #     if self.__client is None:
+    #         pass
+    #     pass
+
+    def get_books(self, query:dict, page:int=1)-> []:
+        if "page" not in query.keys():
+            query["page"] = page
+        print(query)
+        try:
+            res = requests.get(self.frappe_url, params=query)
+            if res.status_code == 200:
+                res = res.json()
+                return res["message"]
+            return []
+        except Exception as e:
+            print("can't get books from frappe", e)
+            return[]
+        
+    def get_img_from_isbn(self, isbn: str) -> str:
+        if isbn == "https://avatars.githubusercontent.com/u/80196675?v=4":
+            return
+        return f"{self.open_lib_url}/{isbn}-M.jpg"
+
+frappe_client=FrappeLib()
+
+        
+
+
+
